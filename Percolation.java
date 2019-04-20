c2df89dd6305ccc95fa4ece0c77b890add5b0ee1
mbadaz <tapsmuzira@gmail.com>
2019-04-20 15:53:07 +0200

    initial commit

---
+++ Percolation.java
@@ -0,0 +1,162 @@
+/**
+ * Name: Pecolation Date: 20/04/2019
+ *
+ * @author mbadaz (Tapiwa Muzira)
+ * <p>
+ * Description: {@link Percolation} is a data type that models a percolation system through an N*N
+ * sized grid that is based on the Weighted Quick Union data structure.
+ * <p>
+ * A system percolates if there is a path of connected open sites that runs from one end (top/start)
+ * of the system to the other end(bottom/end) of the system.
+ * <p>
+ * For example: A pourous material percolates if a liquid (e.g) water flow from the top through to
+ * the bottom of the material, i.e there is a path of open/unblocked sites that runs trough the
+ * material A composite material made of conducting and non-conducting material percolates if an
+ * electric current can flow from the top/start of the material to the bottom/end of the material,
+ * i.e there is a path of connected conducting materials that runs from the one end of the material
+ * to the other end.
+ * <p>
+ * ***************************************************************************
+ */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private final WeightedQuickUnionUF wqUF;  // the Weighted Quick Union Algorithm
+    private int[] sitesStates;          // holds the states of the sites i.e blocked / open
+    private final int gridDimenSize;
+    private final int virtualTop;
+    private final int virtualBottom;
+
+    /**
+     * @param n The grid dimension size
+     */
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+        wqUF = new WeightedQuickUnionUF(n * n);
+        gridDimenSize = n;
+        sitesStates = new int[n * n];
+        virtualTop = 0;
+        virtualBottom = n * n - 1;
+        for (int i = 0; i < n; i++) {
+            sitesStates[i] = 0;
+        }
+    }
+
+    /**
+     * Opens a blocked site at the given coordinates
+     *
+     * @param row is the y coordinate
+     * @param col is the x coordinates
+     */
+    public void open(int row, int col) {
+        int site = coordinatesToSiteId(row, col);
+        if (isOpen(row, col)) return;
+        connectSite(site);
+    }
+
+    /**
+     * Calculates the site index ID from coordinates
+     *
+     * @param row is the y coordinate
+     * @param col is the x coordinate
+     */
+    private int coordinatesToSiteId(int row, int col) {
+        int site = (gridDimenSize * (row - 1)) + (col - 1);
+        if (site < 0) throw new IllegalArgumentException();
+        return site;
+    }
+
+    /**
+     * Checks if a sites is opened
+     *
+     * @param row is the y coordinate
+     * @param col is the x coordinate
+     */
+    public boolean isOpen(int row, int col) {
+        return sitesStates[coordinatesToSiteId(row, col)] == 1;
+    }
+
+    /**
+     * Checks to see whether a site is connected to the top ({@link Percolation#virtualTop})
+     *
+     * @param row is the y coordinate
+     * @param col is the x coordinate
+     * @return boolean
+     */
+    public boolean isFull(int row, int col) {
+        int site = coordinatesToSiteId(row, col);
+        return wqUF.find(site) == wqUF.find(virtualTop);
+    }
+
+    /**
+     * Counts the number of open sites
+     *
+     * @return number of open sites
+     */
+    public int numberOfOpenSites() {
+        int n = 0;
+        for (int i : sitesStates) {
+            if (i == 1) {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    /**
+     * Checks whether the system percolates
+     *
+     * @return
+     */
+    public boolean percolates() {
+        return wqUF.find(virtualTop) == wqUF.find(virtualBottom);
+    }
+
+    /**
+     * Connects the site to all of it's adjacent open sites.
+     *
+     * @param site
+     */
+    private void connectSite(int site) {
+        if (sitesStates[site] == 1) return;
+
+        // calculate the adjacent sites
+        int siteAbove = site - gridDimenSize;
+        int siteRight = site + 1;
+        int siteBelow = site + gridDimenSize;
+        int siteLeft = site - 1;
+
+
+        if (site % gridDimenSize != 0 && sitesStates[siteLeft] == 1) {
+            // connect to left site
+            wqUF.union(site, siteLeft);
+        }
+
+        if ((site + 1) % gridDimenSize != 0 && sitesStates[siteRight] == 1) {
+            // connect to right site
+            wqUF.union(site, siteRight);
+        }
+
+        if (siteAbove >= 0 && sitesStates[siteAbove] == 1) {
+            // connect to site above
+            wqUF.union(site, siteAbove);
+        }
+        else if (siteAbove < 0) {
+            // site is in top row so connect to virtual top site
+            if (site != virtualTop) wqUF.union(site, virtualTop);
+        }
+
+        if (siteBelow < gridDimenSize * gridDimenSize - 1 && sitesStates[siteBelow] == 1) {
+            // connect to below site
+            wqUF.union(site, siteBelow);
+        }
+        else if (siteBelow >= gridDimenSize * gridDimenSize) {
+            // site is in bottom row so connect to virtual bottom site
+            if (site != virtualBottom) wqUF.union(site, virtualBottom);
+        }
+
+        sitesStates[site] = 1;
+    }
+
+}
---
+++ PercolationStats.java
@@ -0,0 +1,76 @@
+/**
+ * Name: PecolationStats Date: 20/04/2019
+ *
+ * @author : mbadaz (Tapiwa Muzira)
+ * <p>
+ * Description: {@link PercolationStats} runs Monte Carlo simulation experiments on the {@link
+ * Percolation} data type and calculates the percolation threshold of each experiments. It then
+ * prints the mean, standard deviation and the 95% confidence interval lower and upper bounds
+ * <p>
+ * The percolation threshold value is the probability p* such that when p < p* a random n-by-n grid
+ * almost never percolates, and when p > p*, a random n-by-n grid almost always percolates, provided
+ * that n is sufficiently large. ***************************************************************************
+ */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private double[] experiments;
+    // the results (percolation threshold) of each experiment
+    private final int t;
+
+    /**
+     * Runs T number of experiment on an N*N grid and collects the results (percolation thresholds)
+     * for all the experiments     *
+     *
+     * @param n number of row/columns of the grid
+     * @param t the number of trials/experiments
+     */
+    public PercolationStats(int n, int t) {
+        if (n <= 0 || t <= 0) throw new IllegalArgumentException();
+        experiments = new double[t];
+        this.t = t;
+        runExperiments(this.t, n);
+    }
+
+    private void runExperiments(int trials, int n) {
+        for (int i = 0; i < trials; i++) {
+            // the Percolation data model
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int x = StdRandom.uniform(1, n + 1);
+                int y = StdRandom.uniform(1, n + 1);
+                perc.open(x, y);
+            }
+            experiments[i] = (perc.numberOfOpenSites() * 1.0) / (n * n * 1.0);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(experiments);
+    }                          // sample mean of percolation threshold
+
+    public double stddev() {
+        return StdStats.stddev(experiments);
+    }                        // sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return mean() - (1.96 * (stddev() / Math.sqrt(t)));
+    }                  // low  endpoint of 95% confidence interval
+
+    public double confidenceHi() {
+        return mean() + (1.96 * (stddev() / Math.sqrt(t)));
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percStats = new PercolationStats(n, trials);
+        StdOut.println("Mean: " + percStats.mean());
+        StdOut.println("Stddev: " + percStats.stddev());
+        StdOut.println("95% confidence interval [" + percStats.confidenceLo() +
+                               ", " + percStats.confidenceHi() + "]");
+    }
+}
